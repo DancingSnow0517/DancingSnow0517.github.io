7e1afb926aca10ccaeb417cab8452ae944212684
DancingSnow <1121149616@qq.com>
2023-05-28 20:39:41 +0800

    vuepress test

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    title: 'DancingSnow的博客',
+    description: 'DancingSnow的个人博客',
+    themeConfig: {
+        nav: [
+            {text: '首页', link: '/'},
+            {text: '崩坏：星穹铁道抽卡导出工具', 'link': '/StarRail-gacha'}
+        ]
+    }
+}
